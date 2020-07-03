1185ca1476ec1976238c5d7e3150afb6346e693b
Titanium Hocker <inbox@titaniumhocker.ru>
2020-07-03 17:12:49 +0300

    Updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,94 @@
 # -*- coding: utf-8 -*-
 import pytest
+from click.testing import CliRunner
+
+
+BASE_HERO_DATA = {
+    'name': 'Gaius Iulius Caesar',
+    'godname': 'Mars',
+    'gender': 'male',
+    'level': 37,
+    'max_health': 244,
+    'inventory_max_num': 25,
+    'motto': 'Какие-то букавы',
+    'clan': 'Тестировщики',
+    'clan_position': 'магистр',
+    'alignment': 'добродушный',
+    'bricks_cnt': 378,
+    'pet': {
+        'pet_name': 'Бублик «Обжора»',
+        'pet_class': 'хохмяк',
+        'pet_level':5
+    },
+    'ark_completed_at': None,
+    'arena_won': 1,
+    'arena_lost': 1,
+    'inventory': {},
+    'gold_approx': '',
+    "ark_f": 100,
+    "ark_m": 100
+}
+
+
+BASE_HERO_DATA_TOKEN = {
+    'name': 'Gaius Iulius Caesar',
+    'godname': 'Mars',
+    'gender': 'male',
+    'level': 37,
+    'max_health': 244,
+    'inventory_max_num': 25,
+    'motto': 'Какие-то букавы',
+    'clan': 'Тестировщики',
+    'clan_position': 'магистр',
+    'alignment': 'добродушный',
+    'bricks_cnt': 379,
+    'pet': {
+        'pet_name': 'Бублик «Обжора»',
+        'pet_class': 'хохмяк',
+        'pet_level': 5
+    },
+    'ark_completed_at': None,
+    'arena_won': 1,
+    'arena_lost': 1,
+    'health': 144,
+    'quest_progress': 67,
+    'exp_progress': 66,
+    'godpower': 46,
+    'gold_approx': 'около 3 сотен',
+    'diary_last': 'Врач заверил, что с таким диагнозом мне не будет равных в психической атаке.',
+    'town_name': 'Пустосвятово',
+    'distance': 16,
+    'arena_fight': False,
+    'inventory_num': 14,
+    'quest': 'разогнать парад планет (эпик)',
+    'activatables': []
+}
+
+
+class MockedResponse:
+    status_code = 200
+
+    @staticmethod
+    def json():
+        return BASE_HERO_DATA
+
+
+class MockedResponseWithToken(MockedResponse):
+    @staticmethod
+    def json():
+        return BASE_HERO_DATA_TOKEN
+
+
+@pytest.fixture
+def mocked_get():
+    return lambda *args, **kwargs: MockedResponse()
+
+
+@pytest.fixture
+def mocked_get_t():
+    return lambda *args, **kwargs: MockedResponseWithToken()
+
+
+@pytest.fixture
+def cli_runner():
+    return CliRunner()
---
+++ tests/test_hero.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+import pytest
+
+from gvapi import Hero
+from gvapi import errors
+from gvapi.hero import requests
+
+
+def test_create_without_token(mocked_get, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get)
+    hero = Hero('Mars')
+    assert isinstance(hero, Hero)
+    assert hero.god == 'Mars'
+    assert hero.name == 'Gaius Iulius Caesar'
+
+
+def test_create_with_token(mocked_get_t, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get_t)
+    hero = Hero('Mars', token='aoidhaodwjaodwhaoidw')
+    assert isinstance(hero, Hero)
+    assert hero.is_alive
+
+
+@pytest.mark.parametrize('name', ('is_alive', 'health', 'health_percent', 'gold', 'activatables',
+                                  'is_fighting', 'fight_type', 'aura', 'diary_last', 'distance',
+                                  'exp', 'expired', 'godpower', 'godpower_percent', 'inventory_num',
+                                  'inventory', 'quest_progress', 'quest', 'town_name'))
+def test_tokenized_exceptions(name, mocked_get, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get)
+    hero = Hero('Mars')
+    with pytest.raises(errors.NeedToken):
+        hero.__getattribute__(name)
+
+
+
+def test_basic(mocked_get_t, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get_t)
+    hero = Hero('Mars', token='aoiwdjawodijadw1234')
+    assert hero.raw_data == mocked_get_t().json()
+    assert hero.__repr__() == '<Hero {}>'.format(hero.name)
+    assert hero.__str__() == 'Герой {}'.format(hero.name)
+    assert hero.__getattribute__('_Hero__get_data')() == mocked_get_t().json()
+    assert hero.token.startswith('****') and hero.token.endswith('1234')
+
+    date = hero._last_upd
+    hero._last_upd = datetime.now() - timedelta(hours=1)
+    assert hero.from_last_updated == 60**2
+    hero.sync()
+    assert hero._last_upd != date
+
+    assert hero.threshold == 300
+    hero.threshold = 200
+    assert hero.threshold == 200
+
+    hero.raw_data['gender'] = 'female'
+    assert hero.__str__() == 'Героиня {}'.format(hero.name)
+
+
+def test_nontokenized_attributes(mocked_get, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get)
+    data = mocked_get().json()
+    hero = Hero('Mars')
+
+    assert hero.name == data['name']
+    assert hero.god == data['godname']
+    assert hero.gender == 'муж'
+    assert hero.level == data['level']
+    assert hero.max_health == data['max_health']
+    assert hero.motto == data['motto']
+    assert hero.clan == data['clan']
+    assert hero.clan_pos == data['clan_position']
+    assert hero.max_godpower == 100
+    assert hero.inventory_max_num == data['inventory_max_num']
+    assert hero.words == 0
+    assert hero.ark == (100, 100)
+    assert hero.arena == (data['arena_won'], data['arena_lost'])
+    assert hero.alignment == data['alignment']
+    assert hero.bricks == data['bricks_cnt']
+    assert hero.bricks_percent == data['bricks_cnt'] / 1000
+
+
+def test_tokenized_attributes(mocked_get_t, monkeypatch):
+    monkeypatch.setattr(requests, 'get', mocked_get_t)
+    data = mocked_get_t().json()
+    hero = Hero('Mars', token='aoiwdjaoiwjd')
+    assert hero.is_alive
+    assert hero.health == data['health']
+    assert hero.health_percent == data['health'] / data['max_health']
+    assert hero.gold == data['gold_approx']
+    assert hero.activatables == data['activatables']
+    assert hero.is_fighting == data['arena_fight']
+    assert hero.fight_type == ''
+    assert hero.aura == ''
+    assert hero.diary_last == data['diary_last']
+    assert hero.distance == data['distance']
+    assert hero.exp == data['exp_progress']
+    assert not hero.expired
+    assert hero.godpower == data['godpower']
+    assert hero.godpower_percent == data['godpower'] / 100
+    assert hero.inventory_num == data['inventory_num']
+    assert hero.inventory == (data['inventory_num'],
+                              data['inventory_max_num'],
+                              data['activatables'])
+    assert hero.quest_progress == data['quest_progress']
+    assert hero.quest == data['quest']
+    assert hero.town_name == data['town_name']
